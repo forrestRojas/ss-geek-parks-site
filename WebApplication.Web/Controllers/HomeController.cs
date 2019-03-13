e10a49ceaf1f92de5027be2d9ac8d1ada6bf8552
forrest rojas <forrest.rojas@gmail.com>
2019-03-13 11:52:16 -0400

    Added call to Get parks, and DAO Dependency

--- WebApplication.Web/Controllers/HomeController.cs
+++ WebApplication.Web/Controllers/HomeController.cs
@@ -4,15 +4,26 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication.Web.DAL;
 using WebApplication.Web.Models;
 
 namespace WebApplication.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IParkDAO parkDAO;
+
+        public HomeController(IParkDAO parkDAO)
+        {
+            this.parkDAO = parkDAO;
+
+        }
+
+        [HttpGet]
         public IActionResult Index()
-        {            
-            return View();
+        {
+            IList<ParkData> parks = this.parkDAO.GetParks();
+            return View(parks);
         }       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
