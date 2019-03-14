f26497158530becb1ac72522243fe34046d96372
forrest rojas <forrest.rojas@gmail.com>
2019-03-14 10:14:09 -0400

    add detail vew model

---
+++ WebApplication.Web/Models/ParkDetailVewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Web.Models
+{
+    /// <summary>
+    /// Represents a park detail view model.
+    /// </summary>
+    public class ParkDetailVewModel
+    {
+        /// <summary>
+        /// Gets or Sets the list of daily weather for the park.
+        /// </summary>
+        public IList<DailyWeather> Forecast { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Park.
+        /// </summary>
+        public ParkData Park { get; set; }
+    }
+}
