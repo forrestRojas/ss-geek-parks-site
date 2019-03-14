5e352d16dbb7143cdc409cb7f418c5250f96548d
forrest rojas <forrest.rojas@gmail.com>
2019-03-14 10:13:40 -0400

    moved scripts out of the html

---
+++ WebApplication.Web/wwwroot/js/jquery/sitejquery.js
@@ -0,0 +1,18 @@
+﻿/* Shrinks the site header */
+$(function shrinkSiteHeader() {
+    var $win = $(window);
+
+    $win.scroll(function () {
+        if ($win.scrollTop() == 0)
+            $("#site-name").removeClass("shrink");
+        else {
+            $("#site-name").addClass("shrink");
+        }
+    });
+});
+
+
+$(document).ready(function () {
+    //call here
+    shrinkSiteHeader();
+});
