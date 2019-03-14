dfc1f1ae00ae4789569b1bacef179ff658d2ea91
forrest rojas <forrest.rojas@gmail.com>
2019-03-14 11:39:36 -0400

    add css to details

--- WebApplication.Web/Startup.cs
+++ WebApplication.Web/Startup.cs
@@ -79,7 +79,7 @@ namespace WebApplication.Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}/{code?}");
             });
         }
     }
