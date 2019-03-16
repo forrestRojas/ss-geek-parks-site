1734594b3ff0b8da5dfa3a3f38f20047ba6cd861
forrest rojas <forrest.rojas@gmail.com>
2019-03-16 14:31:44 -0400

    Fixed IO Error. Cusased by Dependencies. You Will need to install .NET Core 2.2 SDK: https://dotnet.microsoft.com/download

--- WebApplication.Tests/DAL/ParkSqlDAOTests.cs
+++ WebApplication.Tests/DAL/ParkSqlDAOTests.cs
@@ -8,26 +8,26 @@ using System.Text;
 using WebApplication.Web.DAL;
 using WebApplication.Web.Models;
 
-namespace WebApplication.Tests.ControllerMethods
+namespace WebApplication.Tests.DAL
 {
     // TODO MOVE To the DAL Test Folder
     [TestClass]
     public class ParkSqlDAOTests
     {
-        Mock<IList<ParkData>> mockParks;
-        Mock<IList<Survey>> mockSurveys;
-        Mock<IParkDAO> mockParkDAO;
+        IList<ParkData> mockParks;
+        IList<Survey> mockSurveys;
+        IParkDAO mockParkDAO;
 
         [TestInitialize]
         public void Initialize()
         {
             const string connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=NPGeek;Integrated Security=True";
 
-            mockParkDAO = Mock.Get(new ParkSqlDAO(connectionString)).As<IParkDAO>();
-            Mock<ISurveyDAO> mockSurveyDAO = Mock.Get(new SurveySqlDAO(connectionString)).As<ISurveyDAO>();
+            mockParkDAO = new ParkSqlDAO(connectionString);
+            ISurveyDAO mockSurveyDAO = new SurveySqlDAO(connectionString);
 
-            mockParkDAO.SetupGet(m => m.GetParks()).Returns(mockParks.Object);
-            mockSurveyDAO.SetupGet(m => m.GetSurveys()).Returns(mockSurveys.Object);
+            mockParks = mockParkDAO.GetParks();
+            mockSurveys = mockSurveyDAO.GetSurveys();
         }
 
         [TestMethod]
@@ -37,10 +37,10 @@ namespace WebApplication.Tests.ControllerMethods
             IDictionary<string, int> expected = new Dictionary<string, int>();
 
             //ACT
-            IDictionary<string, int> actual = mockParkDAO.Object.GetParkSurveyCounts(mockParks.Object, mockSurveys.Object);
+            IDictionary<string, int> actual = mockParkDAO.GetParkSurveyCounts(mockParks, mockSurveys);
 
             //Assert
-            CollectionAssert.AreEquivalent(expected.ToList(), actual.ToList();
+            CollectionAssert.AreEquivalent(expected.ToList(), actual.ToList());
         }
     }
 }
